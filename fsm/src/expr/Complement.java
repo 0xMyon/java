e8586cdec252251bbab3fe24f0de466980c28bc3
0xMyon <0xMyon@gmail.com>
2022-03-30 16:20:39 +0200

    save action

--- fsm/src/expr/Complement.java
+++ fsm/src/expr/Complement.java
@@ -8,37 +8,38 @@ import lang.Type;
 
 public class Complement<T> extends Expression<T> {
 
-	static <T> Expression<T> of(Expression<T> that) {
+	static <T> Expression<T> of(final Expression<T> that) {
 		if (that instanceof Complement) {
 			return ((Complement<T>)that).complement;
 		} else {
 			return new Complement<>(that);
 		}
 	}
-	
-	private Complement(Expression<T> complement) {
+
+	private Complement(final Expression<T> complement) {
 		this.complement = complement;
 	}
-	
+
 	private final Expression<T> complement;
-	
+
 	@Override
-	public <THAT extends Language<THAT, U>, U> THAT convertLanguage(Language.Factory<THAT, U> factory, Function<T, U> function) {
+	public <THAT extends Language<THAT, U>, U> THAT convertLanguage(final Language.Factory<THAT, U> factory, final Function<T, U> function) {
 		return complement.convertLanguage(factory, function).complement();
 	}
-	
+
 	@Override
-	public <THAT extends Type<THAT, U>, U> THAT convertType(Type.Factory<THAT, U> factory, Function<List<T>, U> function) {
+	public <THAT extends Type<THAT, U>, U> THAT convertType(final Type.Factory<THAT, U> factory, final Function<List<T>, U> function) {
 		return complement.convertType(factory, function).complement();
 	}
-	
+
+	@Override
 	public String toString() {
 		return "!["+complement.toString()+"]";
 	}
 
 	@Override
-	public <R> R accept(Visitor<T, R> visitor) {
+	public <R> R accept(final Visitor<T, R> visitor) {
 		return visitor.handle(this);
 	}
-	
+
 }
--- fsm/src/expr/Union.java
+++ fsm/src/expr/Union.java
@@ -12,47 +12,48 @@ import lang.Type;
 public class Union<T> extends Composite<T> {
 
 	@SafeVarargs
-	public static <T> Expression<T> of(Expression<T>... elements) {
+	public static <T> Expression<T> of(final Expression<T>... elements) {
 		return of(Stream.of(elements));
 	}
-	
-	public static <T> Expression<T> of(Stream<Expression<T>> elements) {
+
+	public static <T> Expression<T> of(final Stream<Expression<T>> elements) {
 		return Composite.of(Union.class, elements, Union::new, Collectors.toSet());
 	}
-	
 
-	private Union(Collection<Expression<T>> set) {
+
+	private Union(final Collection<Expression<T>> set) {
 		super(set);
 	}
-	
+
 	@Override
 	public boolean isEmpty() {
 		return elements().isEmpty();
 	}
 
 	@Override
-	public <THAT extends Language<THAT, U>, U> THAT convertLanguage(Language.Factory<THAT, U> factory, Function<T, U> function) {
+	public <THAT extends Language<THAT, U>, U> THAT convertLanguage(final Language.Factory<THAT, U> factory, final Function<T, U> function) {
 		return factory.union(elements().stream().map(x -> x.convertLanguage(factory, function)));
 	}
-	
+
 	@Override
-	public <THAT extends Type<THAT, U>, U> THAT convertType(Type.Factory<THAT, U> factory, Function<List<T>, U> function) {
+	public <THAT extends Type<THAT, U>, U> THAT convertType(final Type.Factory<THAT, U> factory, final Function<List<T>, U> function) {
 		return factory.union(elements().stream().map(x -> x.convertType(factory, function)));
 	}
-	
+
 	@Override
-	public <THAT extends lang.Set<THAT, U>, U> THAT convertSet(lang.Set.Factory<THAT, U> factory, Function<List<T>, U> function) {
+	public <THAT extends lang.Set<THAT, U>, U> THAT convertSet(final lang.Set.Factory<THAT, U> factory, final Function<List<T>, U> function) {
 		return factory.union(elements().stream().map(x -> x.convertSet(factory, function)));
 	}
-	
+
+	@Override
 	public String toString() {
 		return "{"+elements().stream().map(Object::toString).reduce((a,b) -> a+", "+b).orElse("")+"}";
 	}
-	
-	
+
+
 	@Override
-	public <R> R accept(Visitor<T, R> visitor) {
+	public <R> R accept(final Visitor<T, R> visitor) {
 		return visitor.handle(this);
 	}
-	
+
 }
