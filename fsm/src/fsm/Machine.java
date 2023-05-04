23adff82d0c0578797a069aa836270d0c5da8393
0xMyon <0xMyon@gmail.com>
2023-05-04 22:38:13 +0200

    removed unused imports

--- fsm/src/fsm/Machine.java
+++ fsm/src/fsm/Machine.java
@@ -15,7 +15,6 @@ import java.util.Vector;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import expr.Expression;
